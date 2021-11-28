07b18b68bd73d04b0c37991dd334a1eadca707a0
DanielMolloyUON <DanielTMolloy919@gmail.com>
2021-11-28 16:28:58 +1100

    Able to collect list of sub-directories

--- source_compare.java
+++ source_compare.java
@@ -2,14 +2,17 @@
  * source_compare
  */
 
+import java.io.File;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 
 public class source_compare {
 
     public static void main(String[] args) {
         // if (args.length != 1) { // if the user didn't supply one argument  
-        //     if (!isValidPath(args[0])) { // if the user didn't give a valid path
+        //     if (!is_valid_path(args[0])) { // if the user didn't give a valid path
         //         System.out.println("Proper usage is: source_compare <submissions_folder>");
         //         return;
         //     }
@@ -17,10 +20,31 @@ public class source_compare {
 
         String parent_folder = args[0];
 
-                
+        method_test(parent_folder);
+
+        // List<Submission> submission_set = submission_generator(parent_folder);
+
+        
     }
 
-    public static boolean isValidPath(String path) {
+    // private static List<Submission> submission_generator(String path) {
+
+    //     File[] directories = new File(path).listFiles(File::isDirectory);
+
+    //     System.out.println(Arrays.toString(directories))
+
+    //     List<Submission> placeholder;
+    //     return placeholder;
+
+    // }
+
+    public static void method_test(String path)  {
+        File[] directories = new File(path).listFiles(File::isDirectory);
+
+        System.out.println(Arrays.toString(directories));
+    }
+
+    public static boolean is_valid_path(String path) {
         try {
             Paths.get(path);
         } catch (InvalidPathException | NullPointerException ex) {
